b70b1f9ceb7e2c1a10d722730e19e8d6939dd0f5
ronething <axingfly@gmail.com>
2020-02-09 21:26:33 +0800

    feat: queue

---
+++ Queue/src/com/ronething/Main.java
@@ -0,0 +1,17 @@
+package com.ronething;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+        }
+        System.out.println(queue);
+
+        queue.dequeue();
+
+        System.out.println(queue);
+
+    }
+}
