953c0c361fbcc7b0214a47ec2a8db9667afdaafe
ronething <axingfly@gmail.com>
2020-02-10 22:53:46 +0800

    feat: compare array queue and loop queue

--- Queue/src/com/ronething/ArrayQueue.java
+++ Queue/src/com/ronething/ArrayQueue.java
@@ -49,4 +49,17 @@ public class ArrayQueue<E> implements Queue<E> {
         res.append("] tail");
         return res.toString();
     }
+
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+        }
+        System.out.println(queue);
+
+        queue.dequeue();
+
+        System.out.println(queue);
+
+    }
 }
--- Queue/src/com/ronething/Main.java
+++ Queue/src/com/ronething/Main.java
@@ -1,17 +1,32 @@
 package com.ronething;
 
+
+import java.util.Random;
+
 public class Main {
 
+    private static double testQueue(Queue<Integer> q, int OpCount) {
+
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < OpCount; i++) q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        for (int i = 0; i < OpCount; i++) q.dequeue();
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
     public static void main(String[] args) {
-        ArrayQueue<Integer> queue = new ArrayQueue<>();
-        for (int i = 0; i < 10; i++) {
-            queue.enqueue(i);
-        }
-        System.out.println(queue);
+        int OpCount = 100000;
 
-        queue.dequeue();
-
-        System.out.println(queue);
+//        for (int i = 0; i < 10; i++) {
+            LoopQueue<Integer> loopQueue = new LoopQueue<>();
+            System.out.println("loop queue time: " + testQueue(loopQueue, OpCount));
+            ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+            System.out.println("array queue time: " + testQueue(arrayQueue, OpCount));
+//        }
 
     }
 }
