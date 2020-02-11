c280ba4fd4d10637bf3ff562e408e2d4329471fd
ronething <axingfly@gmail.com>
2020-02-11 22:18:57 +0800

    fix: add and addFirst operation

--- LinkedList/src/com/ronething/LinkedList.java
+++ LinkedList/src/com/ronething/LinkedList.java
@@ -23,4 +23,47 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(E e) {
+//        Node n = new Node(e);
+//        n.next = head;
+//        head = n;
+        head = new Node(e, head);
+        size++;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) throw new IllegalArgumentException("Add failed. Illegal index.");
+
+        if (index == 0) addFirst(e);
+        else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) prev = prev.next;
+//            Node n = new Node(e);
+//            n.next = prev.next;
+//            prev.next = n;
+
+            prev.next = new Node(e, prev.next);
+            size++;
+        }
+
+    }
+
+
 }
