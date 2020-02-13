61f69bc6c410a95502daec8d3a6b056b2d392acc
ronething <axingfly@gmail.com>
2020-02-13 23:59:56 +0800

    feat: linkedListStack

---
+++ LinkedList/src/com/ronething/LinkedListStack.java
@@ -0,0 +1,53 @@
+package com.ronething;
+
+public class LinkedListStack<E> implements Stack<E> {
+
+    private LinkedList<E> linkedList;
+
+    public LinkedListStack() {
+        linkedList = new LinkedList<>();
+    }
+
+    @Override
+    public int getSize() {
+        return linkedList.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return linkedList.isEmpty();
+    }
+
+    @Override
+    public void push(E e) {
+        linkedList.addFirst(e);
+    }
+
+    @Override
+    public E pop() {
+        return linkedList.removeFirst();
+    }
+
+    @Override
+    public E peek() {
+        return linkedList.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Stack: top");
+        res.append(linkedList);
+        return res.toString();
+
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        for (int i = 0; i < 5; i++) {
+            linkedListStack.push(i);
+        }
+
+        System.out.println(linkedListStack);
+    }
+}
