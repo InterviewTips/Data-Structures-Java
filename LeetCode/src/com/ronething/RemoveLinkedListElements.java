138637f3ab95f627aef0b444e500169d8cc1ef2f
ronething <axingfly@gmail.com>
2020-02-15 15:12:41 +0800

    fix: method

--- LeetCode/src/com/ronething/RemoveLinkedListElements.java
+++ LeetCode/src/com/ronething/RemoveLinkedListElements.java
@@ -58,13 +58,16 @@ public class RemoveLinkedListElements {
         public ListNode removeElements(ListNode head, int val) {
             if (head == null) return null;
 
-            ListNode res = removeElements(head.next, val);
-            if (head.val == val) {
-                return res;
-            } else {
-                head.next = res;
-                return head;
-            }
+//            ListNode res = removeElements(head.next, val);
+//            if (head.val == val) {
+//                return res;
+//            } else {
+//                head.next = res;
+//                return head;
+//            }
+            // 简洁写法
+            head.next = removeElements(head.next, val);
+            return head.val == val ? head.next : head;
         }
 
     }
