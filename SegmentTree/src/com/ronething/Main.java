cbeda1dce3fd353e60c542d654332c6c43464ef5
ronething <axingfly@gmail.com>
2020-02-28 23:13:51 +0800

    feat: segment tree

--- SegmentTree/src/com/ronething/Main.java
+++ SegmentTree/src/com/ronething/Main.java
@@ -3,6 +3,16 @@ package com.ronething;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+        SegmentTree<Integer> segmentTree = new SegmentTree<>(nums, new Merger<Integer>() {
+            @Override
+            public Integer merge(Integer a, Integer b) {
+                return a + b;
+            }
+        });
+
+        System.out.println(segmentTree);
+
+
     }
 }
--- SegmentTree/src/com/ronething/SegmentTree.java
+++ SegmentTree/src/com/ronething/SegmentTree.java
@@ -4,8 +4,11 @@ public class SegmentTree<E> {
 
     private E[] tree; // 满二叉树
     private E[] data;
+    private Merger<E> merger;
 
-    public SegmentTree(E[] arr) {
+    public SegmentTree(E[] arr, Merger<E> merger) {
+
+        this.merger = merger;
         data = (E[]) new Object[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
@@ -13,6 +16,8 @@ public class SegmentTree<E> {
         }
 
         tree = (E[]) new Object[4 * arr.length]; // 4n capacity
+
+        buildSegmentTree(0, 0, arr.length - 1);
 
     }
 
@@ -36,4 +41,42 @@ public class SegmentTree<E> {
         return 2 * index + 2;
     }
 
+
+    // 索引，左右区间
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+        if (l == r) { // 只剩下一个节点
+            tree[treeIndex] = data[l];
+            return;
+        }
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        int mid = l + (r - l) / 2;
+
+        buildSegmentTree(leftTreeIndex, l, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("[");
+        for (int i = 0; i < tree.length; i++) {
+            if (tree[i] != null) {
+                res.append(tree[i]);
+            } else {
+                res.append("null");
+            }
+
+            if (i != tree.length - 1) {
+                res.append(",");
+            }
+
+        }
+        res.append("]");
+
+        return res.toString();
+    }
 }
