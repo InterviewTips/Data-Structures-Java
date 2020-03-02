9edb95fbe090c453c22fd8ad7a3f109a770334b5
ronething <axingfly@gmail.com>
2020-03-02 22:47:29 +0800

    feat: trie init and add op

---
+++ Trie/src/com/ronething/Trie.java
@@ -0,0 +1,49 @@
+package com.ronething;
+
+import java.util.TreeMap;
+
+public class Trie {
+
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    // 获取单词数量
+    public int getSize() {
+        return size;
+    }
+
+    // 添加一个单词
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            cur.next.putIfAbsent(c, new Node());
+            cur = cur.next.get(c); // 下个节点一定会走到
+        }
+
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+
+}
