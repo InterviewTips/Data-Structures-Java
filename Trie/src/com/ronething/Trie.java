3b1e0feb7e2f39a9792fd1647c362c7c6a2d9a0d
ronething <axingfly@gmail.com>
2020-03-03 23:39:30 +0800

    fix: trie

--- Trie/src/com/ronething/Trie.java
+++ Trie/src/com/ronething/Trie.java
@@ -45,5 +45,31 @@ public class Trie {
         }
     }
 
+    public boolean contains(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+
+        return cur.isWord;
+    }
+
+
+    public boolean isPrefix(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+
+        return true;
+    }
 
 }
